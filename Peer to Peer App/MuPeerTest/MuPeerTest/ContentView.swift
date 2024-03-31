e8e58caf3e9ef07f9a8a3a09c9fd214bebfb2961
Paige Sun <paige.sun.dev@gmail.com>
2024-03-31 03:39:45 -0700

    Make networking objects ObservableObjects.

--- Peer to Peer App/MuPeerTest/MuPeerTest/ContentView.swift
+++ Peer to Peer App/MuPeerTest/MuPeerTest/ContentView.swift
@@ -5,38 +5,41 @@ import Combine
 
 struct ContentView: View {
     @StateObject public var peersVm = PeersVm.shared
-    
+    @StateObject private var playerNetworking = PlayerNetworking<SendableEntity>(defaultSendable: SendableEntity(sender: "", point: CGPoint(x: 200, y: 200)))
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             PeersView(peersVm)
-            DraggableCircle()
+            DraggableCircle(entity: $playerNetworking.entity)
         }
         .padding()
+        .task {
+            // Get new entity
+            playerNetworking.listen { entity in
+                //  print("Received new entity", entity)
+            }
+        }
     }
 }
 
 struct DraggableCircle: View {
-    @StateObject private var playerNetworking = PlayerNetworking<SendableEntity>(defaultSendable: SendableEntity(sender: "", point: CGPoint(x: 200, y: 200)))
-
+    @Binding var entity: SendableEntity
+    
     var body: some View {
         Circle()
             .frame(width: 50, height: 50)
             .foregroundColor(.blue)
-            .position(playerNetworking.entity.point)
+            .position(entity.point)
             .gesture(
                 DragGesture()
                     .onChanged { value in
-                        playerNetworking.entity = SendableEntity(sender: PeersVm.shared.playerId, point: value.location)
+                        entity = SendableEntity(sender: PeersVm.shared.playerId, point: value.location)
                     }
             )
-            .task {
-                playerNetworking.listen { entity in
-                     print("Received new entity", entity)
-                }
-            }
+            
     }
 }
 
--- Peer to Peer App/MuPeerTest/MuPeerTest/SendableEntity/Networking.swift
+++ Peer to Peer App/MuPeerTest/MuPeerTest/SendableEntity/Networking.swift
@@ -16,8 +16,11 @@ class Weather {
 }
 
 class PlayerNetworking<Sendable: NetworkedEntity>: ObservableObject {
-    let playerId: String
 
+    public lazy var myName: PeerName = {
+        return peersController.myName
+    }()
+    
     private let peersController = PeersController.shared
     
     private var cancellables = Set<AnyCancellable>()
@@ -31,19 +34,19 @@ class PlayerNetworking<Sendable: NetworkedEntity>: ObservableObject {
     init(defaultSendable: Sendable) {
         self.entity = defaultSendable
         
-        playerId = peersController.myName
+        myName = peersController.myName
         peersController.peersDelegates.append(self)
     }
 
     func send(_ sendable: Sendable) {
-        if sendable.sender == playerId {
+        if sendable.sender == myName {
             peersController.sendMessage(sendable, viaStream: false)
         }
     }
     
     func listen(_ callback: @escaping (Sendable) -> Void) {
         $entity.sink { [weak self] recievedSendable in
-            if let self = self, recievedSendable.sender != playerId {
+            if let self = self, recievedSendable.sender != myName {
                 callback(recievedSendable)
             }
         }.store(in: &cancellables)
@@ -53,7 +56,7 @@ class PlayerNetworking<Sendable: NetworkedEntity>: ObservableObject {
 extension PlayerNetworking: PeersControllerDelegate {
     public func received(data: Data, viaStream: Bool) -> Bool {
        if let receivedEntity = try? JSONDecoder().decode(Sendable.self, from: data) {
-            if receivedEntity.sender != playerId {
+            if receivedEntity.sender != myName {
                 entity = receivedEntity
             }
             return true
