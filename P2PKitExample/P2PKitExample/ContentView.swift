4a9adbb82e3680752261723dc083562e072f15fc
Paige Sun <paige.sun.dev@gmail.com>
2024-04-22 12:22:02 -0400

    Create a new blank P2P app.

---
+++ P2PKitExample/P2PKitExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  P2PKitExample
+//
+//  Created by Paige Sun on 4/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
